415947b3afa725c90e1172b419f7ca0f261e461d
Emilio Pears <emilio.pdea@gmail.com>
2023-10-22 12:48:58 +0200

    feat: Add events to event page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,6 +2,9 @@ import PageLayout from "@/components/PageLayout";
 import Image from "next/image";
 import React from "react";
 import automationbounty from "../assets/automationbounty.png";
+import getintocybersecurity from "../assets/getintocybersecurity.png";
+import beginnerbountyseries from "../assets/beginnerbountyseries.png";
+import informationdisclosure from "../assets/informationdisclosure.png";
 import cvehunting from "../assets/cvehunting.png";
 import cybercrimeinvseting from "../assets/cybercrimeinvseting.png";
 import getstartedwithoscp from "../assets/getstartedwithoscp.png";
@@ -47,20 +50,12 @@ const events: EventPosts[] = [
     attendeesCount: "SANJAY SINGH | 132 attendees",
   },
   {
-    image: getstartedwithoscp,
-    date: "Sat, Jul 8, 2023, 10:00AM IST",
-    title: "How to get started with OSCP",
+    image: cybercrimeinvseting,
+    date: "Sun, Aug 27, 2023, 7:00PM IST",
+    title: "cybercrime investigation using OSINT and dark web operations",
     description:
-      "Usman Shah shared his incredible journey of becoming an Offensive Security Certified Professional (OSCP). From exploring the value of OSCP to unlocking career opportunities, his insights provided valuable guidance for aspiring cybersecurity professionals.",
-    attendeesCount: "Usman Shah | 122 attendees",
-  },
-  {
-    image: automationbounty,
-    date: "Sat, Jul 22, 2023, 6:00PM IST",
-    title: "Automating Bug Bounty",
-    description:
-      "In this exciting session, esteemed speaker S Rahul a highly accomplished Cyber Security Analyst, and skilled Bug Bounty Hunter, delved into the secrets of automating bug bounty workflows. From privacy and data protection to penetration testing, Rahul's expertise in various cybersecurity domains enriched the session for both seasoned bug hunters and aspiring enthusiasts",
-    attendeesCount: "S Rahul | 122 attendees",
+      "As the Team Leader at Trinity Infotech Solutions, Ashwin Ragav is dedicated to harnessing the power of Open Source Intelligence (OSINT) techniques for effective cybercrime investigation. His insights promise to shed light on innovative approaches to gather and analyze digital footprints, bolstering our defense against modern cyber threats.",
+    attendeesCount: "Ashwin Ragav | 122 attendees",
   },
   {
     image: web3andbugbounty,
@@ -72,12 +67,44 @@ const events: EventPosts[] = [
     attendeesCount: "Rudra Singh | 122 attendees",
   },
   {
-    image: cybercrimeinvseting,
-    date: "Sun, Aug 27, 2023, 7:00PM IST",
-    title: "cybercrime investigation using OSINT and dark web operations",
+    image: informationdisclosure,
+    date: "Sun, Aug 13, 2023, 4:30PM IST",
+    title: "Information Disclosure Methodology",
     description:
-      "As the Team Leader at Trinity Infotech Solutions, Ashwin Ragav is dedicated to harnessing the power of Open Source Intelligence (OSINT) techniques for effective cybercrime investigation. His insights promise to shed light on innovative approaches to gather and analyze digital footprints, bolstering our defense against modern cyber threats.",
-    attendeesCount: "Ashwin Ragav | 122 attendees",
+      "A resounding round of applause and heartfelt thanks to our remarkable speaker, Agnibha Dutta (AnonY0gi), for a captivating session on Information Disclosure Methodology. Your insights, expertise, and dedication truly ignited a passion for cybersecurity in all of us!",
+    attendeesCount: "Agnibha Dutta (AnonY0gi) | 81 attendees",
+  },
+  {
+    image: getintocybersecurity,
+    date: "Sat, Jul 29, 2023, 5:00PM IST",
+    title: "How to Get Into Cyber Security - Building your path and crafting an impactful resume",
+    description:
+      "Are you curious about breaking into the dynamic field of Cyber Security? Our esteemed Cyber Security Consultant, Sanjay Singh, will guide you on how to kickstart your journey and build a rewarding career in cybersecurity. In this session, you'll gain essential insights into starting your cybersecurity journey, crafting an impactful resume that stands out to recruiters, navigating certifications and skills to boost your employability, and acquiring practical experience and networking opportunities.",
+    attendeesCount: "Sanjay Singh| 46 attendees",
+  },
+  {
+    image: automationbounty,
+    date: "Sat, Jul 22, 2023, 6:00PM IST",
+    title: "Automating Bug Bounty",
+    description:
+      "In this exciting session, esteemed speaker S Rahul a highly accomplished Cyber Security Analyst, and skilled Bug Bounty Hunter, delved into the secrets of automating bug bounty workflows. From privacy and data protection to penetration testing, Rahul's expertise in various cybersecurity domains enriched the session for both seasoned bug hunters and aspiring enthusiasts",
+    attendeesCount: "S Rahul | 122 attendees",
+  },
+  {
+    image: beginnerbountyseries,
+    date: "Sun, Jul 16, 2023, 6:00PM IST",
+    title: "Beginner Bounty Series",
+    description:
+      "Join Bug Bounty Brigade and OWASP Kaithal for an educational bug-hunting session. Our speaker, Shaik Arif Ali, will focus on discovering low-hanging fruits in bug hunting, making it ideal for beginners. This collaborative event provides a supportive environment for bug bounty enthusiasts, whether you're new to the field or looking to refine your skills. Expect hands-on examples and a community-driven approach to learning.",
+    attendeesCount: "Shaik Arif Ali | 54 attendees",
+  },
+  {
+    image: getstartedwithoscp,
+    date: "Sat, Jul 8, 2023, 10:00AM IST",
+    title: "How to get started with OSCP",
+    description:
+      "Usman Shah shared his incredible journey of becoming an Offensive Security Certified Professional (OSCP). From exploring the value of OSCP to unlocking career opportunities, his insights provided valuable guidance for aspiring cybersecurity professionals.",
+    attendeesCount: "Usman Shah | 122 attendees",
   },
 ];
 
